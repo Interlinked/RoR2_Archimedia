6cd91aaa9f034cee51be3e5f44dce74a3a386b08
interlinked <elijahbrockman09@gmail.com>
2025-12-29 19:58:10 -0800

    ui doesnt work

--- Archimedia/UI.cs
+++ Archimedia/UI.cs
@@ -10,12 +10,12 @@ namespace Archimedia
         {
             SceneManager.activeSceneChanged += OnSceneChanged;
             "ARCHIMEDIA_MENU_NAME".Add("Elite Void Archimedia");
-            "BOSSRUSH_HOVERDESC".Add("Fight through a harder run with a randomized loadout.");
+            "ARCHIMEDIA_HOVERDESC".Add("Fight through a harder run with a randomized loadout.");
 
             SceneManager.activeSceneChanged += OnSceneChanged;
             On.RoR2.ConsoleFunctions.SubmitCmd += (orig, self, str) =>
             {
-                if(self.GetComponent<HGButton>() && self.GetComponent<HGButton>().hoverToken == "BOSSRUSH_HOVERDESC")
+                if(self.GetComponent<HGButton>() && self.GetComponent<HGButton>().hoverToken == "ARCHIMEDIA_HOVERDESC")
                 {
                     if(str == "transition_command \"gamemode ArchimediaRun; host 0;\"")
                     {
@@ -32,5 +32,29 @@ namespace Archimedia
         
 
 
+    
+            private static void OnSceneChanged(Scene prev, Scene next) {
+            if (next.name == "title") {
+                GameObject gameObject = GameObject.Find("MainMenu");
+                Transform transform = gameObject.transform.Find("MENU: Extra Game Mode/ExtraGameModeMenu/Main Panel/GenericMenuButtonPanel/JuicePanel/GenericMenuButton (Eclipse)");
+                if (transform)
+                {
+                    GameObject button = UnityEngine.Object.Instantiate(transform.gameObject, transform.parent);
+                    button.GetComponent<LanguageTextMeshController>().token = "ARCHIMEDIA_MENU_NAME";
+                    ConsoleFunctions consoleFunctions = button.GetComponent<ConsoleFunctions>();
+                    HGButton component = button.GetComponent<HGButton>();
+                    component.hoverToken = "ARCHIMEDIA_HOVERDESC";
+                    component.onClick.RemoveAllListeners();
+                    component.onClick.AddListener(delegate
+                    {
+                        consoleFunctions.SubmitCmd("transition_command \"gamemode archimediaRun; host 0;\"");
+                    });
+
+                }
+                else {
+                    Debug.Log("Could not find transform.");
+                }
+            }
+        }
     }
 }
